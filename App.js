ad221ff0661a58ad969ffcd7d0ec47c762f94f9c
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-06-30 18:11:15 -0400

    added buttons and divs

--- App.js
+++ App.js
@@ -44,6 +44,13 @@ class ReactApp extends React.Component {
         <div id="timer-label"> Session </div>
         {/*// User Story #8: I can see an element with corresponding id="time-left". NOTE: Paused or running, the value in this field should always be displayed in mm:ss format (i.e. 25:00).*/}
         <div id="time-left"> 25:00 </div>
+
+        <button id="start_stop"> </button> 
+        <button id="reset"></button>
+
+ {/* // User Story #11: When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length" should return to 5, the value within id="session-length" should return to 25, and the element with id="time-left" should reset to its default state. */}
+
+
       </div>
     );
   }
@@ -62,11 +69,7 @@ root.render(
 
 
 
-// User Story #9: I can see a clickable element with a corresponding id="start_stop".
 
-// User Story #10: I can see a clickable element with a corresponding id="reset".
-
-// User Story #11: When I click the element with the id of reset, any running timer should be stopped, the value within id="break-length" should return to 5, the value within id="session-length" should return to 25, and the element with id="time-left" should reset to its default state.
 
 // User Story #12: When I click the element with the id of break-decrement, the value within id="break-length" decrements by a value of 1, and I can see the updated value.
 
